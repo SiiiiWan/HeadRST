6f418d39e0d14a2c71d7e3dab6b2055ada1c78c1
Wang <wangh90@lancaster.ac.uk>
2025-06-07 15:53:00 +0100

    implemented scaled HOMER

--- Assets/Scripts/ManipulationBehavoirs/GazeNPinchOrigin.cs
+++ Assets/Scripts/ManipulationBehavoirs/GazeNPinchOrigin.cs
@@ -6,5 +6,8 @@ public class GazeNPinchOrigin : ManipulationTechnique
     {
         Vector3 deltaPos = HandPosition.GetInstance().GetDeltaHandPosition(usePinchTip: true);
         target.position += deltaPos * Vector3.Distance(target.position, Camera.main.transform.position);
+
+        Quaternion deltaRot = HandPosition.GetInstance().GetDeltaHandRotation(usePinchTip: true);
+        target.rotation = deltaRot * target.rotation;
     }
 }
