8a5fc931894785f260ccaef63d87cda83efb2cc4
Wang <wangh90@lancaster.ac.uk>
2025-06-15 13:21:18 +0100

    also reset manipulated bunny

--- Assets/Scripts/StudyControl.cs
+++ Assets/Scripts/StudyControl.cs
@@ -7,7 +7,19 @@ public class StudyControl : Singleton<StudyControl>
     public ManipulationTechnique ManipulationBehavior;
     public ManipulatableObject GrabbedObject;
 
+    public GameObject ManipulationBunny;
     public List<GameObject> DockingTargets;
+
+    private Vector3 _startingPosition;
+    private Quaternion _startingRotation;
+
+    void Start()
+    {
+        _startingPosition = ManipulationBunny.transform.position;
+        _startingRotation = ManipulationBunny.transform.rotation;
+
+        
+    }
 
     void Update()
     {
@@ -19,11 +31,14 @@ public class StudyControl : Singleton<StudyControl>
     }
 
     public void Reset()
-    {   
+    {
         foreach (GameObject bunny in DockingTargets)
         {
             bunny.SetActive(true);
         }
+        
+        ManipulationBunny.transform.position = _startingPosition;
+        ManipulationBunny.transform.rotation = _startingRotation;
     }
 
 }
