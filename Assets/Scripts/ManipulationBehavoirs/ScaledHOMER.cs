8ec2fb6a1af0c442b1bfffac20495b28ccee1f30
Wang <wangh90@lancaster.ac.uk>
2025-06-27 14:52:15 +0100

    updated scaled homer

--- Assets/Scripts/ManipulationBehavoirs/ScaledHOMER.cs
+++ Assets/Scripts/ManipulationBehavoirs/ScaledHOMER.cs
@@ -9,17 +9,15 @@ public class ScaledHOMER : ManipulationTechnique
     public float TorsoOffset = 0.35f; // Offset from the camera to the torso
 
     private Vector3 lastHandPosition;
-    private float scalingConstant = 0.15f; //TODO: fine parameters from papers
-    private float minVelocityThreshold = 0.01f;
+    public float scalingConstant = 0.15f; //TODO: fine parameters from papers
+    public float minVelocityThreshold = 0.01f;
 
     public override void OnSingleHandGrabbed(Transform target)
     {
         HandPosition handData = HandPosition.GetInstance();
-        Vector3 handPos = handData.GetHandPosition(usePinchTip: false);
+        Vector3 handPos = handData.GetHandPosition(usePinchTip: true);
 
-        Vector3 torsoPosition = Camera.main.transform.position + Vector3.down * TorsoOffset;
-
-        StartManipulation_HOMER(torsoPosition, handPos, target.position);
+        StartManipulation_HOMER(GetCurrentTorsoPosition(), handPos, target.position);
 
         lastHandPosition = handPos;
     }
@@ -28,63 +26,65 @@ public class ScaledHOMER : ManipulationTechnique
     {
         HandPosition handData = HandPosition.GetInstance();
 
-        Vector3 torsoPosition = Camera.main.transform.position + Vector3.down * TorsoOffset;
-        TorsoPositionIndicator.position = torsoPosition;
-        HandPositionIndicator.position = handData.GetHandPosition(usePinchTip: false);
-        Vector3 currentHandPosition = handData.GetHandPosition(usePinchTip: false);
+        Vector3 handPos_delta = handData.GetDeltaHandPosition(usePinchTip: true);
+        Quaternion handRot_delta = handData.GetDeltaHandRotation(usePinchTip: true);
+        Vector3 handPos = handData.GetHandPosition(usePinchTip: true);
 
-        //TODO: update hand position tracking
 
-        Vector3 handVelocity = (currentHandPosition - lastHandPosition) / Time.deltaTime;
+        Vector3 handVelocity = handPos_delta / Time.deltaTime;
         float handSpeed = handVelocity.magnitude;
+        print($"Hand Speed: {handSpeed}");
 
         float scaleFactor = Mathf.Min(1.2f, handSpeed / scalingConstant);
-        if (handSpeed < minVelocityThreshold)
-        {
-            scaleFactor = 0f; // Freeze motion for precision
-        }
+        if (handSpeed < minVelocityThreshold)  scaleFactor = 0f;
 
-        Vector3 scaledHandMovement = handVelocity.normalized * handSpeed * scaleFactor * Time.deltaTime;
+        Vector3 scaledHandMovement = handPos_delta * scaleFactor;
         Vector3 scaledHandPosition = lastHandPosition + scaledHandMovement;
 
-        lastHandPosition = currentHandPosition;
+        lastHandPosition = handPos;
 
-        // target.position = UpdateObjectPosition_HOMER(scaledHandPosition);
-        target.position = UpdateObjectPosition_HOMER(currentHandPosition);
-
+        target.position = UpdateObjectPosition_HOMER(scaledHandPosition);
+        // target.position = UpdateObjectPosition_HOMER(handPos);
 
         Quaternion deltaRot = HandPosition.GetInstance().GetDeltaHandRotation(usePinchTip: true);
         target.rotation = deltaRot * target.rotation;
     }
 
-    private Vector3 torsoPosition;
-    private Vector3 initialHandPosition;
-    private Vector3 initialObjectPosition;
-    private float distanceHandToTorso;
-    private float distanceObjectToTorso;
-    private Vector3 offsetVector;
+    private Vector3 _torsoPosition_init;
+    private Vector3 _handPosition_init;
+    private Vector3 _objectPosition_init;
+    private float _distanceHandToTorso_init;
+    private float _distanceObjectToTorso_init;
+    private Vector3 _offsetVector;
 
     public void StartManipulation_HOMER(Vector3 torso, Vector3 hand, Vector3 objectPosition)
     {
-        torsoPosition = torso;
-        initialHandPosition = hand;
-        initialObjectPosition = objectPosition;
+        // Difference between HOMER and visual gain is that HOMER handle the depth translation
+        _torsoPosition_init = torso;
+        _handPosition_init = hand;
+        _objectPosition_init = objectPosition;
 
-        distanceHandToTorso = Vector3.Distance(torsoPosition, initialHandPosition);
-        distanceObjectToTorso = Vector3.Distance(torsoPosition, initialObjectPosition);
+        _distanceHandToTorso_init = Vector3.Distance(_torsoPosition_init, _handPosition_init);
+        _distanceObjectToTorso_init = Vector3.Distance(_torsoPosition_init, _objectPosition_init);
 
-        Vector3 rayDirection = (initialHandPosition - torsoPosition).normalized;
-        Vector3 expectedObjectPosition = torsoPosition + rayDirection * distanceObjectToTorso;
-        offsetVector = initialObjectPosition - expectedObjectPosition;
+        Vector3 torso_to_hand_Direction_init = (_handPosition_init - _torsoPosition_init).normalized;
+        Vector3 expectedObjectPosition = _torsoPosition_init + torso_to_hand_Direction_init * _distanceObjectToTorso_init;
+        _offsetVector = _objectPosition_init - expectedObjectPosition;
+    }
+
+    Vector3 GetCurrentTorsoPosition()
+    {
+        return Camera.main.transform.position + Vector3.down * TorsoOffset;
     }
 
     public Vector3 UpdateObjectPosition_HOMER(Vector3 currentHandPosition)
     {
-        float currentHandDistance = Vector3.Distance(torsoPosition, currentHandPosition);
-        float scaledDistance = (distanceObjectToTorso / distanceHandToTorso) * currentHandDistance;
+        Vector3 currentTorsoPosition = GetCurrentTorsoPosition();
+        float distanceHandToTorso_current = Vector3.Distance(currentTorsoPosition, currentHandPosition);
+        float scaledDistance = _distanceObjectToTorso_init / _distanceHandToTorso_init * distanceHandToTorso_current;
 
-        Vector3 bodyToHand = (currentHandPosition - torsoPosition).normalized;
-        Vector3 objectPosition = torsoPosition + bodyToHand * scaledDistance + offsetVector;
+        Vector3 torso_to_hand_Direction_current = (currentHandPosition - currentTorsoPosition).normalized;
+        Vector3 objectPosition = currentTorsoPosition + torso_to_hand_Direction_current * scaledDistance + _offsetVector;
         return objectPosition;
     }
     
