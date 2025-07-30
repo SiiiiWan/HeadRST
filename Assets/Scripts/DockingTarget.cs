a46d93cb81ec060ca10dfe8b74a9ac4ff1d2562b
Wang <wangh90@lancaster.ac.uk>
2025-07-30 15:36:13 +0100

    Allow inverse alignement

--- Assets/Scripts/DockingTarget.cs
+++ Assets/Scripts/DockingTarget.cs
@@ -17,8 +17,11 @@ public class DockingTarget : MonoBehaviour
         if (grabbedObject != null)
         {
             DistanceToGrabbedObj = Vector3.Distance(transform.position, grabbedObject.transform.position);
-            OrientationDifference = Quaternion.Angle(transform.rotation, grabbedObject.transform.rotation);    
-            _poseAligned = DistanceToGrabbedObj < 0.1f && OrientationDifference < 30f;
+            OrientationDifference = Quaternion.Angle(transform.rotation, grabbedObject.transform.rotation);
+            // _poseAligned = DistanceToGrabbedObj < 0.1f && OrientationDifference < 10f;
+            _poseAligned = DistanceToGrabbedObj < 0.05f && IsAxisAligned(grabbedObject.transform, 5f);
+            // _poseAligned = DistanceToGrabbedObj < 0.015f && IsAxisAligned(grabbedObject.transform, 3.5f);
+
         }
 
         // transform.GetComponent<Outline>().OutlineColor = _poseAligned? Color.green : Color.red;
@@ -30,6 +33,24 @@ public class DockingTarget : MonoBehaviour
     public bool IsPoseAligned()
     {
         return _poseAligned;
+    }
+    
+    public bool IsAxisAligned(Transform grabbedTransform, float angleThreshold)
+    {
+        // Get local axes for both objects
+        Vector3[] targetAxes = { transform.right, transform.up, transform.forward };
+        Vector3[] grabbedAxes = { grabbedTransform.right, grabbedTransform.up, grabbedTransform.forward };
+
+        for (int i = 0; i < targetAxes.Length; i++)
+        {
+            float angle = Vector3.Angle(targetAxes[i], grabbedAxes[i]);
+            if (angle >= angleThreshold && Mathf.Abs(angle - 180f) >= angleThreshold)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 
 }
