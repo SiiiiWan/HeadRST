5679ead8659832352ef08e98a278623d1727dff4
Wang <wangh90@lancaster.ac.uk>
2025-08-10 14:13:23 +0100

    updated orientation alignement

--- Assets/Scripts/DockingTarget.cs
+++ Assets/Scripts/DockingTarget.cs
@@ -9,7 +9,7 @@ public class DockingTarget : MonoBehaviour
     public float PositionDifference;
     public float OrientationDifference;
     public List<Transform> Wedges = new List<Transform>();
-    public float PositionAlignmentThreshold {get; private set;} = 0.20f;
+    public float PositionAlignmentThreshold {get; private set;} = 0.25f;
     public float OrientationAlignmentThreshold {get; private set;} = 10f;
 
     void Update()
@@ -20,7 +20,6 @@ public class DockingTarget : MonoBehaviour
 
         if (taskObject != null)
         {
-            OrientationDifference = Quaternion.Angle(transform.rotation, taskObject.transform.rotation);
             // _poseAligned = PositionDifference < 0.1f && OrientationDifference < 10f;
             _poseAligned = IsOrientationAligned() && IsPositionAligned();
 
@@ -47,26 +46,30 @@ public class DockingTarget : MonoBehaviour
     {
         GameObject taskObject = StudyControl.GetInstance().ObjectToBeManipulated;
         PositionDifference = Vector3.Distance(transform.position, taskObject.transform.position);
-        return PositionDifference < PositionAlignmentThreshold;
+        return PositionDifference < PositionAlignmentThreshold* transform.localScale.x;
     }
-
+ 
     public bool IsPositionAligned_Double()
     {
         GameObject taskObject = StudyControl.GetInstance().ObjectToBeManipulated;
         PositionDifference = Vector3.Distance(transform.position, taskObject.transform.position);
-        return PositionDifference < PositionAlignmentThreshold * 2;
+        return PositionDifference < PositionAlignmentThreshold * 2 * transform.localScale.x;
     }
 
     public bool IsOrientationAligned()
     {
         GameObject taskObject = StudyControl.GetInstance().ObjectToBeManipulated;
-        return IsAxisAligned(taskObject.transform, OrientationAlignmentThreshold);
+        OrientationDifference = Quaternion.Angle(transform.rotation, taskObject.transform.rotation);
+        return OrientationDifference < OrientationAlignmentThreshold;
+        // return IsAxisAligned(taskObject.transform, OrientationAlignmentThreshold);
     }
 
     public bool IsOrientationAligned_Double()
     {
         GameObject taskObject = StudyControl.GetInstance().ObjectToBeManipulated;
-        return IsAxisAligned(taskObject.transform, OrientationAlignmentThreshold * 2);
+        OrientationDifference = Quaternion.Angle(transform.rotation, taskObject.transform.rotation);
+        return OrientationDifference < OrientationAlignmentThreshold * 2;
+        // return IsAxisAligned(taskObject.transform, OrientationAlignmentThreshold * 2);
     }
 
     public bool IsAxisAligned(Transform grabbedTransform, float angleThreshold)
@@ -86,6 +89,7 @@ public class DockingTarget : MonoBehaviour
 
         return true;
     }
+
     
     public void SetActiveWedges(bool isActive)
     {
