89c7c9f09f4d9c49f38dfe8f516e842706304f82
Haopeng Wang <h.wang73@lancaster.ac.uk>
2025-12-12 18:09:56 +0000

    added slime

--- Assets/Scripts/Magic Game/Slime.cs
+++ Assets/Scripts/Magic Game/Slime.cs
@@ -1,15 +1,10 @@
 using UnityEngine;
 
-
-public class Bat : Monster
+public class Slime : Monster
 {
-
     public override void Move()
     {
-        transform.LookAt(Camera.main.transform.position);
+        transform.LookAt(new Vector3(Camera.main.transform.position.x, 0, Camera.main.transform.position.z));
         transform.Translate(Vector3.forward * Speed * Time.deltaTime);
     }
-
 }
-
-
