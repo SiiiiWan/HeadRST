2b0e79a8359132bc7d660dbcc201d7960ee2d4bf
Haopeng Wang <h.wang73@lancaster.ac.uk>
2026-01-28 15:53:45 +0000

    implemented gogo

--- Assets/Scripts/VirtualHand Provider/GoGo.cs
+++ Assets/Scripts/VirtualHand Provider/GoGo.cs
@@ -1,27 +1,46 @@
 using UnityEngine;
+using static OVRPlugin;
 
 public class GoGo : VirtualHandProvider
 {
     [Range(0f, 1f)] public float D = 0.475f;
     [Range(0f, 1f)] public float k = 1/6f;
-    
-    Vector3 torsoOffset = new Vector3(0, -0.35f, 0);
-    // public override Vector3 UpdatePivot(Vector3 currentPosition)
-    // {
-    //     UpdateDataSource();
 
-    //     Vector3 Rr = HandData.RightHandPosition - (HeadData.HeadPosition + torsoOffset);
-        
-    //     if(Rr.magnitude >= D)
-    //     {
-    //         return HandData.RightHandPosition + Rr.normalized * Mathf.Pow((Rr.magnitude - D) * 100, 2) * k;
-    //         // return HandData.RightHandPosition + Rr.normalized * 3;
 
-    //     }
-    //     else
-    //     {
-    //         return HandData.RightHandPosition;
-    //     }
-    // }
+    public override void UpdateVirtualHandPoses()
+    {
+        // Get Tracking Data
+        UpdateDataSource();
+
+        // // Get Hand Data
+        Vector3 rightHandPosition = HandData.RightHandPosition;
+        Vector3 leftHandPosition = HandData.LeftHandPosition;
+
+        // // Get Torso Data
+        Posef torsoPose = BodyData.BodyState?.JointLocations[5].Pose ?? default;
+        Vector3 torsoPosition = new Vector3(torsoPose.Position.x, torsoPose.Position.y, -torsoPose.Position.z);
+
+        Vector3 Rr = rightHandPosition - torsoPosition;
+        Vector3 Lr = leftHandPosition - torsoPosition;
+
+        Vector3 rightVirtualHandPosition = rightHandPosition;
+        Vector3 leftVirtualHandPosition = leftHandPosition;;
+
+        if(Rr.magnitude >= D)
+        {
+            rightVirtualHandPosition = rightHandPosition + Rr.normalized * Mathf.Pow((Rr.magnitude - D) * 100, 2) * k;
+        }
+
+        if(Lr.magnitude >= D)
+        {
+            leftVirtualHandPosition = leftHandPosition + Lr.normalized * Mathf.Pow((Lr.magnitude - D) * 100, 2) * k;
+        }
+
+
+        VirtualHandPoses = new VirtualDoubleHandPoses(
+            new Pose(leftVirtualHandPosition, HandData.LeftHandRotation),
+            new Pose(rightVirtualHandPosition, HandData.RightHandRotation)
+        );
+    }
 
 }
